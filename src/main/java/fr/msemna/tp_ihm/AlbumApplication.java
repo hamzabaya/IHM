61d136d3bdb105b579cd62fe52358d4dd1097d1a
Mehdi Chouaf <mehdi.chouaf@uha.fr>
2025-01-21 14:13:11 +0100

    remove comments

--- src/main/java/fr/msemna/tp_ihm/AlbumApplication.java
+++ src/main/java/fr/msemna/tp_ihm/AlbumApplication.java
@@ -1,29 +1,28 @@
 package fr.msemna.tp_ihm;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class AlbumApplication extends Application {
 
     @Override
     public void start(Stage stage) {
         try {
-            // Charger le fichier FXML
+            
             FXMLLoader fxmlLoader = new FXMLLoader(AlbumApplication.class.getResource("hello-view.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
 
-            // Titre de la fenêtre
+
             stage.setTitle("Album Photo - TP 2024");
 
-            // Définir la scène et afficher la fenêtre
+
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
-            // Gestion des erreurs de chargement du FXML
             e.printStackTrace();
             System.err.println("Erreur lors du chargement du fichier FXML : " + e.getMessage());
         }
